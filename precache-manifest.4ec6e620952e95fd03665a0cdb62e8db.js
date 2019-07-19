2c3627caec83f8cf95e264ba2e52329da00261fc
Aleksandar Sajdovski <Aleksandar.Sajdovski@netcetera.com>
2019-07-20 01:32:46 +0200

    Updates

---
+++ precache-manifest.4ec6e620952e95fd03665a0cdb62e8db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7980d5fd73b3202e7617f07de58f46cd",
+    "url": "/reactjsapp/index.html"
+  },
+  {
+    "revision": "a1c8278c4028e683d9b6",
+    "url": "/reactjsapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactjsapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a1c8278c4028e683d9b6",
+    "url": "/reactjsapp/static/js/main.57e65e4f.chunk.js"
+  },
+  {
+    "revision": "0cefb3fee6d8830887f6",
+    "url": "/reactjsapp/static/js/runtime~main.78d389b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjsapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
